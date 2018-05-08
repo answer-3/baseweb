56ac766d8755208bb27cb3911d3d7a99948a7d55
answer-3 <answer-3@qq.com>
2018-05-08 17:19:49 +0800

    hello

---
+++ src/main/java/com/lizhan/core/util/Test.java
@@ -0,0 +1,13 @@
+package com.lizhan.core.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+    private static  Logger logger = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        logger.info("哈哈哈哈");
+    }
+
+}
